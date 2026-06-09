900b1d347de83b9b72bbb59363333b6eede2ab91
Mark Engelmann <engelmann.mark@gmail.com>
2026-06-09 11:46:38 -0500

    Add epigraph shadows

---
+++ src/scripts/epigraph.js
@@ -0,0 +1,18 @@
+const epi = document.querySelector('.epigraph > p');
+
+let shadowStr = '';
+const numShadows = 50;
+const distance = 150;
+
+for (let i = 0; i < numShadows; i++) {
+  let shadow = 'rgb(from var(--white) r g b / 0.1)';
+  for (let j = 0; j < 2; j++) {
+    const randFloat = Math.random() * distance * 2 - distance;
+    shadow += ` ${randFloat}px`;
+  }
+
+  if (i < numShadows - 1) { shadow += ',' };
+  shadowStr += shadow;
+}
+
+epi.style.textShadow = shadowStr;
